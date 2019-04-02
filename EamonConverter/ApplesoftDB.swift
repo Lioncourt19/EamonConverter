e77c4098842e7174e0d27eeed5ff7d6771cfafa2
Josh de Lioncourt <contact@joshdelioncourt.com>
2019-04-02 14:52:18 -0400

    Separated String extension into its own .swift

--- EamonConverter/ApplesoftDB.swift
+++ EamonConverter/ApplesoftDB.swift
@@ -8,39 +8,6 @@
 
 import Foundation
 
-extension String {
-    func group(of n: Int) -> [String] {
-        let chars = Array(self)
-        return stride(from: 0, to: chars.count, by: n).map {
-            String(chars[$0..<min($0+n, chars.count)])
-        }
-    }
-
-    func inputs(_ quantity: Int) -> [String]? {
-        var total = quantity
-        let inputs = self.components(separatedBy: "\r")
-        if inputs.count < total { return nil }
-        var subset = [String]()
-        for i in 0..<total {
-            subset.append(inputs[i])
-        }
-        return subset
-    }
-
-    func collapseWhitespace() -> String {
-        let stripped = self.components(separatedBy: .whitespaces)
-        var result = ""
-
-        for component in stripped {
-            if component.count > 0 {
-                result += component
-                if component != stripped.last { result += " " }
-            }
-        }
-        return result
-    }
-}
-
 class ApplesoftDB {
 
     private var _rawData = String()
